30f81c9dac234433dfe06e7033c5d83c741fe850
Edson Vanderlei <edson@pagare.app>
2023-08-21 17:01:37 -0300

    Commit Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,170 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import Logo from "../../public/JuiceLogo.png";
+import MangoJuiceSmallSize from "../../public/MangoJuiceSmallSize.png";
+import MangoJuiceBigSize from "../../public/MangoJuiceBigSize.png";
+import LycheeJuiceSmallSize from "../../public/LycheeJuiceSmallSize.png";
+import LycheeJuiceBigSize from "../../public/LycheeJuiceBigSize.png";
+import OrangeJuiceSmallSize from "../../public/OrangeJuiceSmallSize.png";
+import MangoBackground from "../../public/MangoBackground.png";
+import LycheeBackground from "../../public/LycheeBackground.png";
+import OrangeBackground from "../../public/OrangeBackground.png";
+import OrangeJuiceBigSize from "../../public/OrangeJuiceBigSize.png";
+import { Poppins } from "next/font/google";
+import { useCallback, useEffect, useRef, useState } from "react";
+
+const poppins = Poppins({
+  weight: "500",
+  subsets: ["latin"],
+});
 
 export default function Home() {
+  const rotateRef = useCallback((node: HTMLDivElement) => {
+    if (node) {
+      node.style.height = `${node.clientWidth}px`;
+      window.addEventListener("resize", () => {
+        node.style.height = `${node.clientWidth}px`;
+      });
+    }
+  }, []);
+
+  const tabItem = useRef<HTMLDivElement>(null);
+
+  const Fruits = [
+    {
+      bg: MangoBackground.src,
+      image: MangoJuiceSmallSize.src,
+      title: "Mango",
+      text: "Pure Mango Bliss: Crafted from the finest, sun-ripened mangoes, our Mango Juice is bursting with the natural sweetness and succulent goodness of this exotic fruit. Every glass of our juice embodies the essence of a sun-kissed mango orchard, delivering an unparalleled taste experience that will leave you craving for more.",
+      color: "bg-orange-600",
+    },
+    {
+      bg: LycheeBackground.src,
+      image: LycheeJuiceSmallSize.src,
+      title: "Lychee",
+      text: "Taste of Tropical Bliss: Our Lychee Juice is a celebration of the unique lychee fruit's natural essence. With a harmonious blend of sweetness and floral undertones, it offers a distinctive flavor that will leave you longing for more. Our expert juicers skillfully extract the purest juices from the juiciest lychees, ensuring every drop is brimming with authentic taste and goodness.",
+      color: "bg-pink-600",
+    },
+    {
+      bg: OrangeBackground.src,
+      image: OrangeJuiceSmallSize.src,
+      title: "Orange",
+      text: "Harvesting Nature's Each glass of Orange Juice is a celebration of nature's bountiful harvest. Handpicked from lush orchards, the ripest and juiciest oranges are carefully selected to ensure the finest quality. With each bottle, you experience the essence of sun-drenched groves and the care put into crafting this thirst-quenching masterpiece.",
+      color: "bg-yellow-600",
+    },
+  ];
+
+  const [index, setIndex] = useState<number>(0);
+
+  const rotate = ["rotate-90", "rotate-0", "-rotate-90"];
+  const tabRef = useCallback(
+    (node: HTMLDivElement) => {
+      if (node) {
+        node.style.width = `${tabItem.current?.clientWidth}px`;
+        node.style.transform = `translateX(${
+          (node.clientWidth + 20) * index
+        }px)`;
+      }
+    },
+    [index]
+  );
+
+  const imageRef = useCallback((node: HTMLDivElement) => {
+    if (node) {
+      window.addEventListener("resize", () => {
+        node.style.height = `${node.clientWidth}px`;
+      });
+      node.style.height = `${node.clientWidth}px`;
+    }
+  }, []);
+
+  const mainRef = useCallback(
+    (node: HTMLDivElement) => {
+      if (node) {
+        node.style.backgroundImage = `url(${Fruits[index].bg})`;
+      }
+    },
+    [index]
+  );
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main
+      ref={mainRef}
+      className="bg-orange-600 w-screen h-screen overflow-hidden bg-no-repeat bg-cover transition-all duration-[2000] px-52"
+    >
+      <div className="flex flex-col h-full w-full m-auto">
+        <div className="flex justify-between items-center py-3">
+          <div className="w-40 relative h-16 shrink-0">
+            <Image src={Logo.src} alt="Logo Juice" fill={true} />
+          </div>
+          <div className="flex items-center gap-12  text-white text-3xl">
+            <p className="cursor-pointer">Flavors</p>
+            <p className="cursor-pointer">Find us</p>
+            <p className="cursor-pointer">About Us</p>
+            <p className="cursor-pointer">Franchise</p>
+          </div>
+          <button className="border-4 text-white border-white px-6 text-2xl py-2">
+            ONLINE STORE
+          </button>
+        </div>
+        {}
+        <div className="flex items-end flex-1">
+          <div className="flex flex-col z-10 h-full justify-center gap-8 text-white flex-1">
+            <p className="text-9xl">{Fruits[index].title}</p>
+            <p className="text-xl pr-16" style={poppins.style}>
+              {Fruits[index].text}
+            </p>
+            <div className="flex items-center w-full gap-5 relative">
+              {Fruits.map((_, i) => {
+                return (
+                  <div ref={tabItem}>
+                    <Image
+                      className="cursor-pointer"
+                      onClick={() => setIndex(i)}
+                      key={i}
+                      src={_.image}
+                      alt="Juice"
+                      width={200}
+                      height={200}
+                    />
+                  </div>
+                );
+              })}
+              <div
+                ref={tabRef}
+                className="h-1 absolute transition-transform duration-700 w-full rounded-xl bg-white -bottom-1 left-0"
+              ></div>
+            </div>
+          </div>
+          <div className="h-full flex-1 relative flex items-end">
+            <div
+              ref={rotateRef}
+              className={
+                "w-[65vw] transition-transform  absolute duration-700 bg-opacity-30 bg-white -bottom-[110%] left-1/2 -translate-x-1/2 rounded-full " +
+                rotate[index]
+              }
+            >
+              <div
+                ref={imageRef}
+                className="-top-[50%] w-[75%] rotate-0 absolute left-1/2 -translate-x-1/2 "
+              >
+                <Image src={LycheeJuiceBigSize.src} alt="Juice" fill={true} />
+              </div>
+              <div
+                ref={imageRef}
+                className="top-1/2 -left-[50%]  w-[75%] -rotate-90 absolute -translate-y-1/2 "
+              >
+                <Image src={MangoJuiceBigSize.src} alt="Juice" fill={true} />
+              </div>
+              <div
+                ref={imageRef}
+                className="top-1/2 -right-[50%]  w-[75%] absolute rotate-90 -translate-y-1/2  "
+              >
+                <Image src={OrangeJuiceBigSize.src} alt="Juice" fill={true} />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
